8b7c4de751e3265ca5bf41f5d357016450a106be
Billy Nguyen <tuq33158@temple.edu>
2025-03-18 09:55:36 -0400

    Initial React Commit
    Creating initial React project. Please check if you guys can run the page!

---
+++ whoowesmemoney/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          If you can see this then it is working.
+        </p>
+        <p>
+          To run "cd whoowesmemoney"
+          then "npm start"
+        </p>
+        <p>
+          "Ctrl C" to end
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
