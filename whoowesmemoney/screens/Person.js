b9997dcd9eca71cfe0a2ca6dc857bc8d021b5f5f
tun53840 <tun53840@temple.edu>
2025-04-16 23:47:05 -0400

    Added basic data storage for debtors.

--- whoowesmemoney/screens/Person.js
+++ whoowesmemoney/screens/Person.js
@@ -1,44 +1,74 @@
 import React, { useState, useRef, useCallBack } from 'react';
 import { View, Button, Image, Text, ScrollView, StyleSheet, TextInput } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
 
 let full = "First Last";
 let totalOwed = 0.00;
+let peopleOwed = " ";
+let token = "tokenator";
 // var listOwed = [];
 // var amountOwed = [];
 var people = [];
 function newerPerson(word, number){
-  this.name = word;
+  this.fullname = word;
   this.owed = number;
 }
 
 
 export default function Person({ navigation }){
-  const [name, setName] = useState('John Doe');
-  const inputRef = useRef();
+  const [nameHolder, setNameHolder] = useState('');
   const [person, setPerson] = useState([{}]);
-  const onPressButton = () =>{
-    newPerson(name, totalOwed); 
+  const [debtors, setDebtors] = useState([]);
+  getDebtor();  
+
+  const handleSubmit = (debtorName) => {
+    //setName(debtorName);
+    if (debtorName === '') {
+        alert("Please Enter Something!");
+    } else {
+      let full = debtorName;
+    newPerson(full, totalOwed); 
+    //setDebtors(people);
+    storeData();
+    }
   };
-  const handleSubmit = () => {
-    let textInputValue = inputRef.text;
+  const handleGet = () => {
+    peopleOwed = people[0];
   };
+
+  const storeData = async () =>{
+    try{
+      await AsyncStorage.setItem(
+        token,
+        JSON.stringify(people),
+      );
+    console.log("success!");
+    } catch (error){
+      // Error
+      console.log(error);
+    }
+  }
+
+  
+
+  const readList = 'PLACEHOLDER FOR DISPLAYING DEBTOR LIST';
   
     return (
         <ScrollView style={styles.scroll}>
               <View style={styles.container}>
-              <Button title="Back to Home" onPress={() => navigation.navigate('Home')}/>
+                <Button title="Back to Home" onPress={() => navigation.navigate('Home')}/>
                 <Text>Enter debtor's name:</Text>
                 <TextInput 
                 style={styles.input}
-                value={name}
                 placeholder={"e.g. John Doe"}
-                onChangeText={(text) => setName(text)}/>
-                <Button title="Make new Debtor" onPress={onPressButton}/>
-                <Button title="What Is Owed" onPress={personCheck(full)}/>
-                <Text style={styles.label}>This person owes me in total:</Text>
-                <Text style={styles.label}>{totalOwed}</Text>
+                onChangeText={(text) => setNameHolder(text)}
+                />
+                <Button title="Make new Debtor" onPress={()=>handleSubmit(nameHolder)}/>
+                <Button title="Who Owes Me Money" onPress={()=>handleGet()}/>
+                <Text style={styles.label}>These are my debtors and what they owe:</Text>
+                <Text style={styles.label}>{readList}</Text>
               </View>
          </ScrollView>
     )
@@ -47,7 +77,7 @@ export default function Person({ navigation }){
 
 function newPerson(thing, number){
   for(let i = 0; i<people.length; i++){
-    if(thing == people[i].name){
+    if(thing == people[i].fullname){
       return;
     }
   }
@@ -55,13 +85,25 @@ function newPerson(thing, number){
     people.push(debtor);
     console.log(people);
 };
-  function personCheck(full){
-    for(let i = 0;i < people.length; i++){
-      if(full == people[i].fullName){
-        totalOwed = people[i].owed;
-      }
+
+  /* function getDebtors(){
+    for(let i = 0;i< people.length; i++){
+      // peopleOwed = localStorage.getItem(people[i]);
     }
-  }
+  }; */
+
+  async function getDebtor(){
+    try {
+      const value = await AsyncStorage.getItem(token);
+      if (value !== null) {
+        // We have data!!
+        people = (JSON.parse(value));
+      }
+    } catch (error) {
+      // Error retrieving data
+      console.log("failure in getting debtors");
+    }
+  }; 
 
   const styles = StyleSheet.create({
     scroll: { flex: 1, backgroundColor: '#fff' },
